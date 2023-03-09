7ade0c705fe9fa051f274b9907afb864c703de53
Blessman-Newton <blessmannewton0@gmail.com>
2023-03-09 15:25:41 -0800

    0-positive_or_negative.py

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -5,7 +5,7 @@ if number > 0:
     print(f'{number:d} is positive')
 elif number < 0:
     print(f'{number:d} is negative')
-elif number == 0
+elif number == 0:
     print(f'{number:d} is zero')
 else:
     print(f'{number:d} is wrong type')
