b77bfce849c3c43c19205b7cb38cd6523e3d8bbe
Blessman-Newton <blessmannewton0@gmail.com>
2023-07-26 16:02:43 +0100

    Update 0-readme

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,9 +3,5 @@
 const fs = require('fs');
 
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
-  if (err) {
-    console.log(err);
-  } else {
-    process.stdout.write(data);
-  }
+  console.log(err || data);
 });
